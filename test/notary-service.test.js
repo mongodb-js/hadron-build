d6e22d6553850142db0e08c54daf9099fbe25aed
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2017-01-12 17:44:32 -0500

    COMPASS-544: Linux (#63)
    * COMPASS-544: First crack at porting notary-service client to js
    
    * Pull in `.rpm` requires from Atom
    
    See https://github.com/atom/atom/pull/13536
    
    Should resolve missing file reported by @pzrq at:
    https://github.com/mongodb-js/hadron-build/pull/57/files#r94353590
    
    * libXss.so.1 -> libXScrnSaver
    
    * rpm categories and deb section
    
    * notary-service testing
    
    * alpha channel
    
    `dev` is a special channel in Linux so instead replace `dev` with
    `alpha-${timestamp}` in the version when building on CI
    
    * Add tests for dev -> alpha channel on evergreen
    
    * sign rpm via notary
    
    * Yarn lock
    
    * :shirt:
    
    * debugging
    
    * Check deb files on success
    
    * :shirt:
    
    * debugging travis
    
    * correct lintian path
    
    * `libsecret-1` -> `libsecret`
    
    * Fix auth_token generation
    
    Use fixture data captured in COMPASS-639 to track down that we were not
    generating the correct `salt` for `pbkdf2` generation. I redacted the
    original fixture data and added a new test for `getSalt(secret)`.
    
    This was fixed by passing correct arguments to lodash `split()` and
    `join()`: empty strings.
    
    * Allow `export NO_ASAR=1` to opt-out of asar
    
    * COMPASS-638 First crack at deb signing workaround
    
    * debugging deb
    
    * More notary debugging
    
    * Sign linux tarballs with notary
    
    * No special case for rhel
    
    * `electron-installer-run` -> `execa`
    
    * Revert "`electron-installer-run` -> `execa`"
    
    This reverts commit 0bc6664b1f25a3d2ed238691211776c31157f1b3.
    
    * skip deb on travis

--- test/notary-service.test.js
+++ test/notary-service.test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 
 describe('notary-service', function() {
   const secret = 'A-------------Z';
-  const now = 1484238590217;
+  const now = '2017-01-12T21:18:31.384Z';
 
   it('should generate the right salt', function() {
     expect(notary.getSalt(secret)).to.equal('Z-------------A');
@@ -13,6 +13,6 @@ describe('notary-service', function() {
   it('should generate the auth token', function() {
     const token = notary.generateAuthToken(secret, now);
     expect(token).to.be.a('string');
-    expect(token).to.equal('8628593d83fa61ee5538770de6e2ab98d1d40cbe1484238590217');
+    expect(token).to.equal('039177c6856119edeb56823ec4efb8913d0d03852017-01-12T21:18:31.384Z');
   });
 });
